25e4e7106948e177f28e348d8eec4eaa96004bc6
Coen ten Thije Boonkkamp <coen@coenttb.com>
2025-12-23 14:50:13 +0100

    Update swift-html-rendering to 0.1.15 and swift-css to 0.6.1
    Uses swift-async-algorithms-fork to resolve dependency conflicts.

--- Package.swift
+++ Package.swift
@@ -15,17 +15,17 @@ let package = Package(
         .library(name: "PDF HTML Rendering", targets: ["PDF HTML Rendering"])
     ],
     dependencies: [
-        .package(url: "https://github.com/coenttb/swift-html-rendering", from: "0.1.12"),
+        .package(url: "https://github.com/coenttb/swift-html-rendering", from: "0.1.15"),
         .package(url: "https://github.com/coenttb/swift-pdf-rendering", from: "0.6.0"),
         .package(url: "https://github.com/coenttb/swift-copy-on-write", from: "0.3.1"),
-        .package(url: "https://github.com/coenttb/swift-css", from: "0.3.1"),
+        .package(url: "https://github.com/coenttb/swift-css", from: "0.6.1"),
         .package(url: "https://github.com/swift-standards/swift-html-standard", from: "0.1.6"),
-        .package(url: "https://github.com/swift-standards/swift-css-standard", from: "0.1.6"),
-        .package(url: "https://github.com/swift-standards/swift-w3c-css", from: "0.1.0"),
-        .package(url: "https://github.com/swift-standards/swift-standards", from: "0.20.0"),
-        .package(url: "https://github.com/swift-standards/swift-iso-9899", from: "0.1.0"),
-        .package(url: "https://github.com/swift-standards/swift-iec-61966", from: "0.1.0"),
-        .package(url: "https://github.com/pointfreeco/swift-snapshot-testing", from: "1.18.0"),
+        .package(url: "https://github.com/swift-standards/swift-css-standard", from: "0.1.7"),
+        .package(url: "https://github.com/swift-standards/swift-w3c-css", from: "0.3.0"),
+        .package(url: "https://github.com/swift-standards/swift-standards", from: "0.21.0"),
+        .package(url: "https://github.com/swift-standards/swift-iso-9899", from: "0.2.3"),
+        .package(url: "https://github.com/swift-standards/swift-iec-61966", from: "0.1.3"),
+        .package(url: "https://github.com/pointfreeco/swift-snapshot-testing", from: "1.18.7"),
     ],
     targets: [
         .target(
